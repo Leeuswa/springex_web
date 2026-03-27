a36e47140b5b124355a52bac7dea4a7b2e7bfc39
ynkite <tkddus3521@naver.com>
2026-03-27 21:00:13 +0900

    26-3-27 1. 조회(Read) 기능 완성 2. 삭제(Delete) 기능 완성 3. 수정(Update) 기능 - 1단계: 핵심 DB 처리 (영속성) 4. 수정(Update) 기능 - 2단계: 데이터 검증 및 포맷터 (안정성)

--- src/main/java/com/example/springex_web/mapper/TodoMapper.java
+++ src/main/java/com/example/springex_web/mapper/TodoMapper.java
@@ -8,4 +8,7 @@ public interface TodoMapper {
     String getTime();
     void insert(TodoVO todoVo);
     List<TodoVO> selectAll(); //selectAll();  -> DB에 있는 모든 데이터 조회
+    TodoVO selectOne(Long tno);
+    void delete(Long tno);
+    void update(TodoVO todoVO);
 }
